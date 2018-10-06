7cdf315f741063cc548e79c15bb9998a4d4d1feb
Atiq Rahman <atiqcs@outlook.com>
2018-10-05 19:56:38 -0700

    path validation and setting implemented new class created for demo purpose

--- CodeFormatter/CodeFmt.cs
+++ CodeFormatter/CodeFmt.cs
@@ -2,49 +2,103 @@
 // Use of this source code is governed by a GPL-v3 license
 
 namespace ConsoleApp {
-  using System;
-  using CommandLine;
+using System;
+using CommandLine;
+
+/// <summary>
+/// A Code Formatting Application
+/// 
+/// Documentation (requirements/features/design decisions) at
+/// https://github.com/atiq-cs/CodeFormatter/wiki
+/// 
+/// </summary>
+class CodeFormatter {
+  /// <summary>
+  /// Location of provided source files or directory to process
+  /// 
+  /// Value being empty/null indicats an error state and most methods should not be called in
+  /// error state
+  /// </summary>
+  public string Path { get; private set; }
+  public bool IsDirectory { get; set; }
+
+  public CodeFormatter() {
+  }
+
+  public bool SetPath(string path) {
+    bool fileExists = System.IO.File.Exists(path);
+    if (fileExists || System.IO.Directory.Exists(path)) {
+      Path = path;
+      IsDirectory = fileExists ? false : true;
+      return true;
+    }
+    return false;
+  }
 
   /// <summary>
-  /// A Code Formatting Application
+  /// Location of provided source files or directory to process
   /// 
-  /// Documentation (requirements/features/design decisions) at
-  /// https://github.com/atiq-cs/CodeFormatter/wiki
-  /// 
-  /// Run Example, 
+  /// Value being empty/null indicats an error state and most methods should not be called in
+  /// error state
   /// </summary>
-  class CodeFormatter {
-    public class Options {
-      [Option('t', "replaceTabs", Required = false, HelpText = "Replace tabs with two spaces.")]
-      public bool claShouldReplaceTabs { get; set; }
-    }
-
-    static void Main(string[] args) {
-      Parser.Default.ParseArguments<Options>(args)
-        .WithParsed<Options>(o => {
-          if(o.claShouldReplaceTabs) {
-            Console.WriteLine("Replacing tabs..");
-            return ;
-          } else {
-            Console.WriteLine($"Current Arguments: -v {o.claShouldReplaceTabs}");
-            Console.WriteLine("Quick Start Example!");
-          }
-        });
-      return;
-      // Read each line of the file into a string array. Each element of the
-      // array is one line of the file.
-      // what if source file size is 1 GB or something, try to avoid reading
-      // such files..
-
-      // For debugging from VS
-      // string[] lines = System.IO.File.ReadAllLines(@"D:\Code\CSharp\CodePrettifier\obj\Debug\Ex.cs");
-      // dotnet run: relative to location from where executable is run from
-      string[] lines = System.IO.File.ReadAllLines(@"obj\Debug\Ex.cs");
-      Util utilDemo = new Util();
-
-      /* bool isTab = utilDemo.GetIndentationType();
-      utilDemo.FixIndentation(isTab);
-      int numIndentSpaces = utilDemo.IndentationSettingsFinder(lines); */
-    }
+  public void ReplaceTabs() {
+    if (string.IsNullOrEmpty(Path))
+      return ;
+    Console.WriteLine("Replacing tabs for " + Path + "..");
+    Console.WriteLine("Path is a " + (IsDirectory?"Dir":"File"));
+    // dotnet run: relative to location from where executable is run from
+    string[] lines = System.IO.File.ReadAllLines(@"obj\Debug\Ex.cs");
   }
+
+
+  public void Run() {
+  }
+}
+
+/// <summary>
+/// Entry Point
+/// 
+/// Handles command line
+/// instantiates Code Formatter Instance
+/// and calls required methods based on cmd line provided
+/// 
+/// Run Example, 
+/// </summary>
+class CodeFmtDemo {
+  public class Options {
+    [Option('t', "replaceTabs", Required = false, HelpText = "Replace tabs with two spaces.")]
+    public bool ShouldReplaceTabs { get; set; }
+    [Option('p', "path", Required = true, HelpText = "Location of source file/s.")]
+    public string Path { get; set; }
+  }
+
+  static void Main(string[] args) {
+    var app = new CodeFormatter();
+    Parser.Default.ParseArguments<Options>(args)
+      .WithParsed<Options>(o => {
+        if(o.ShouldReplaceTabs) {
+          if (app.SetPath(o.Path) == false)
+            throw new ArgumentException("Invalid path specified!");
+          app.ReplaceTabs();
+          return ;
+        } else {
+          Console.WriteLine($"Current Arguments: -v {o.ShouldReplaceTabs}");
+          Console.WriteLine("Quick Start Example!");
+        }
+      });
+    return;
+    // Read each line of the file into a string array. Each element of the
+    // array is one line of the file.
+    // what if source file size is 1 GB or something, try to avoid reading
+    // such files..
+
+    // For debugging from VS
+    // string[] lines = System.IO.File.ReadAllLines(@"D:\Code\CSharp\CodePrettifier\obj\Debug\Ex.cs");
+    Util utilDemo = new Util();
+
+    /* bool isTab = utilDemo.GetIndentationType();
+    utilDemo.FixIndentation(isTab);
+    int numIndentSpaces = utilDemo.IndentationSettingsFinder(lines); */
+  }
+}
 }
--- CodeFormatter/Util.cs
+++ CodeFormatter/Util.cs
@@ -14,96 +14,96 @@ using System.Text;
 */
 
 namespace ConsoleApp {
-  class Util {
-    const int MaxNumLinesToProbe = 8;
+class Util {
+  const int MaxNumLinesToProbe = 8;
 
-    public Util() {
+  public Util() {
+  }
+
+  /// <summary>
+  /// Can be space or tabs
+  /// Returns true if tabs
+  /// </summary>
+
+  public bool GetIndentationType(string[] lines) {
+    bool isInsideBlockComment = false;
+    for (int i = 0, oci = 0; oci < MaxNumLinesToProbe && i < lines.Length; i++) {
+      var line = lines[i];
+      if (isBlockCommentStatusToggling(line, isInsideBlockComment))
+        isInsideBlockComment = isInsideBlockComment ? false : true;
+      if (isInsideBlockComment == false)
+        oci++;
+
+
     }
+    return false;
+  }
 
-    /// <summary>
-    /// Can be space or tabs
-    /// Returns true if tabs
-    /// </summary>
-
-    public bool GetIndentationType(string[] lines) {
-      bool isInsideBlockComment = false;
-      for (int i = 0, oci = 0; oci < MaxNumLinesToProbe && i < lines.Length; i++) {
-        var line = lines[i];
-        if (isBlockCommentStatusToggling(line, isInsideBlockComment))
-          isInsideBlockComment = isInsideBlockComment ? false : true;
-        if (isInsideBlockComment == false)
-          oci++;
-
-
+  public int IndentationSettingsFinder(string[] lines) {
+    // Display the file contents by using a foreach loop.
+    Console.WriteLine("Contents:");
+    bool isInsideBlockComment = false;
+    int prevIndentLen = 0, indentLength=0;
+    for (int i=0; i<MaxNumLinesToProbe && i<lines.Length; i++) {
+      var line = lines[i];
+      if (isBlockCommentStatusToggling(line, isInsideBlockComment)) {
+        isInsideBlockComment = isInsideBlockComment ? false : true;
+        Console.WriteLine(line + "\t[inside block comment? " + isInsideBlockComment + "]");
       }
-      return false;
-    }
-
-    public int IndentationSettingsFinder(string[] lines) {
-      // Display the file contents by using a foreach loop.
-      Console.WriteLine("Contents:");
-      bool isInsideBlockComment = false;
-      int prevIndentLen = 0, indentLength=0;
-      for (int i=0; i<MaxNumLinesToProbe && i<lines.Length; i++) {
-        var line = lines[i];
-        if (isBlockCommentStatusToggling(line, isInsideBlockComment)) {
-          isInsideBlockComment = isInsideBlockComment ? false : true;
-          Console.WriteLine(line + "\t[inside block comment? " + isInsideBlockComment + "]");
-        }
-        else { // Use a tab to indent each line of the file.
-          Console.WriteLine(line);
-        }
+      else { // Use a tab to indent each line of the file.
+        Console.WriteLine(line);
+      }
         
-        int len = GetIndentationLength(line, isInsideBlockComment);
-        if (len > 0) {
-          indentLength = len;
-          prevIndentLen = indentLength;
-        }
-      }
-
-      return 0;
-    }
-
-    // Simplified method
-    bool isBlockCommentStatusToggling(string line, bool isInsideBlockComment) {
-      if (isInsideBlockComment) {
-        // /* started in some previous line
-        // do we get an ending */ ?
-        // if last found /* is before last found */ then toggle
-        int startPos = line.LastIndexOf("*/");
-        if (startPos == -1)
-          return false;
-        int endPos = line.LastIndexOf("/*");
-        if (endPos == -1)
-          return true;
-        return (startPos >= endPos + 2);
-      }
-      else {
-        int startPos = line.LastIndexOf("/*");
-        if (startPos == -1)
-          return false;
-        int endPos = line.LastIndexOf("*/");
-        if (endPos == -1)
-          return true;
-        return (startPos <= endPos + 2);
+      int len = GetIndentationLength(line, isInsideBlockComment);
+      if (len > 0) {
+        indentLength = len;
+        prevIndentLen = indentLength;
       }
     }
 
-    /* 
-     * return value
-     * -1 - Error
-     * 0 - no indentation
-     * 
-     */
-    int GetIndentationLength(string line, bool isInsideBlockComment) {
-      if (isInsideBlockComment) {
-        if (line.StartsWith("**"))
-          return -1;
+    return 0;
+  }
 
-      } else {
-        // int indentl
-      }
-      return 0;
+  // Simplified method
+  bool isBlockCommentStatusToggling(string line, bool isInsideBlockComment) {
+    if (isInsideBlockComment) {
+      // /* started in some previous line
+      // do we get an ending */ ?
+      // if last found /* is before last found */ then toggle
+      int startPos = line.LastIndexOf("*/");
+      if (startPos == -1)
+        return false;
+      int endPos = line.LastIndexOf("/*");
+      if (endPos == -1)
+        return true;
+      return (startPos >= endPos + 2);
+    }
+    else {
+      int startPos = line.LastIndexOf("/*");
+      if (startPos == -1)
+        return false;
+      int endPos = line.LastIndexOf("*/");
+      if (endPos == -1)
+        return true;
+      return (startPos <= endPos + 2);
     }
   }
+
+  /* 
+    * return value
+    * -1 - Error
+    * 0 - no indentation
+    * 
+    */
+  int GetIndentationLength(string line, bool isInsideBlockComment) {
+    if (isInsideBlockComment) {
+      if (line.StartsWith("**"))
+        return -1;
+
+    } else {
+      // int indentl
+    }
+    return 0;
+  }
+}
 }
